dddebb3301e3be88a4390643ba4f80f2a645292e
vins-stha <shrs_samar@yahoo.com>
2022-09-24 10:24:11 +0300

    spring setup

---
+++ src/main/java/com/example/ssn_api/SsnApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssn_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsnApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsnApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ssn_api/SsnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssn_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsnApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
